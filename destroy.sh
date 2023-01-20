06a79a17cce4f2cdb0185114a4473f23ae9a6999
Paulo Vitor dos Santos <paulovitor8@gmail.com>
2023-01-20 01:41:45 -0300

    Melhorando Projeto

--- destroy.sh
+++ destroy.sh
@@ -3,5 +3,6 @@
 helm uninstall jenkins -n jenkins
 helm uninstall ingress -n jenkins
 kubectl delete secrets regcred -n jenkins
-kubectl delete -f /app
+kubectl delete -f app/
 kubectl delete -f jenkins/ingress.yaml
+kubectl delete ns jenkins
