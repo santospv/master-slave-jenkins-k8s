ac840929a16ac71c7c8b52b312f7b7bc2302811b
Paulo Vitor dos Santos <paulovitor8@gmail.com>
2023-01-20 01:26:37 -0300

    Mudanca no projeto

--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-helm unistall jenkins -n jenkins
-helm unistall ingress -n jenkins
+helm uninstall jenkins -n jenkins
+helm uninstall ingress -n jenkins
 kubectl delete secrets regcred -n jenkins
 kubectl delete -f /app
+kubectl delete -f jenkins/ingress.yaml
