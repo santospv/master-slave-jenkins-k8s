387ec5c8aa34db782f1111b4de0e306fcc46c206
Paulo Vitor dos Santos <paulovitor8@gmail.com>
2023-01-20 14:12:48 -0300

    Alteracao Projeto

--- first-deploy.sh
+++ first-deploy.sh
@@ -6,4 +6,4 @@ chmod +x secret-docker.sh
 helm install ingress ingress-nginx/ingress-nginx -n jenkins
 helm install jenkins jenkins/jenkins --values jenkins/jenkins.yaml -n jenkins
 kubectl apply -f jenkins/ingress.yaml
-kubectl apply -f app/
+#kubectl apply -f app/
